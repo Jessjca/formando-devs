4278a04480389aba8506810c966ca1778fbb1cd9
Jessjca <juzyres@gmail.com>
2023-03-22 20:13:30 -0300

    base da framework svelte

---
+++ src/lib/components/buttons/FilledButton.svelte
@@ -0,0 +1,43 @@
+<script>
+	/**
+	 * @type string
+	 */
+	export let href;
+	/**
+	 * @type string
+	 */
+	export let color;
+	/**
+	 * @type string
+	 */
+	export let hoverColor;
+</script>
+
+<div>
+	<a {href} style="--color: var({color});--hoverColor: var({hoverColor})"><slot /></a>
+</div>
+
+<style>
+	a {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem 2.4rem;
+		border: none;
+		border-radius: 4rem;
+		background-color: var(--color);
+		color: var(--luminous-white);
+		font-weight: 700;
+		font-size: 1.6rem;
+		line-height: 1.8rem;
+		text-transform: uppercase;
+		gap: 1.6rem;
+		text-decoration: none;
+		transition: background 200ms;
+		width: fit-content;
+	}
+	a:hover {
+		background-color: var(--hoverColor);
+		border: none;
+	}
+</style>
---
+++ src/lib/components/buttons/FloatingButton.svelte
@@ -0,0 +1,23 @@
+<script>
+	/**
+	 * @type string
+	 */
+	export let href;
+</script>
+
+<div>
+	<a {href}><slot /></a>
+</div>
+
+<style>
+	a {
+		color: var(--luminous-white);
+		transition-duration: 0.2s;
+		text-decoration: none;
+		text-transform: uppercase;
+		line-height: 3.1rem;
+	}
+	a:hover {
+		color: var(--primary-orange);
+	}
+</style>
