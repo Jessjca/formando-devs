c18468541b35c55793e1686b716ca7520f81c1c1
Jessjca <juzyres@gmail.com>
2023-04-18 00:00:30 -0300

    Inicialização de projeto angular

--- src/lib/stores/UserStore.ts
+++
@@ -1,7 +0,0 @@
-import { writable } from "svelte/store";
-interface IUser {
-    userName: string;
-    uuid: string;
-}
-const writableStore = writable<IUser | undefined>(undefined)
-export { writableStore }
